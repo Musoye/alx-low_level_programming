87f352fd995efb34caed68ef18e9530420fade38
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 14:10:59 -0700

    9th task ::smile::

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quote, sizeof(quote));
 return (1);
 }
