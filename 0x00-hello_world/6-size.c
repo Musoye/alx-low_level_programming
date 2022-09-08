5a5486513f8ce7b06b084f4a7b515e20c4bf9be8
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 09:07:54 -0700

    7th task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+printf("Size of an float: %i byte(s)\n", sizeof(float));
+}
