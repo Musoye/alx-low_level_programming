a33faad65330680c92a25f3a82ef4d6196c39f3d
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 13:57:24 -0700

    9th task upgraded

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main entry point
  *
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-system("echo 'and that piece of art is useful\" - Dora Korpar\, 2015-10-19'");
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(2,quote,sizeof(quote));
 return (1);
 }
