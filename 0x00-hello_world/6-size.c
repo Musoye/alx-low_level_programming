c12fbc3c704b4672611ef0bcb1d5333f566149b0
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 09:11:01 -0700

    7th task upgraded

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 printf("Size of a char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of an float: %i byte(s)\n", sizeof(float));
 }
