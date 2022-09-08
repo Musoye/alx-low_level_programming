842e5256f4bfb0735251dd65ae292b16b4736db0
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 09:32:41 -0700

    9th task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+return (1);
+}
