87815a69e204fb704bbfa7edfd09a9a8cbfdf213
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-08 08:37:47 -0700

    5th task upgraded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - main entry point
  *
- * return:0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
