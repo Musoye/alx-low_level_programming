0c937b773a826616493ab326264ef70e523637ed
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-09 14:21:12 -0700

    3rd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *Return: Always 0 (Success
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+		putchar (alpha);
+		alpha++;
+		putchar('\n');
+	return (0);
+}
