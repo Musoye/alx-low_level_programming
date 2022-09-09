684056ad83252d7e6cc2cb4b2c692ef2f9676adf
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-09 15:10:15 -0700

    4th

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,11 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-
 	putchar('\n');
-
-        return (0);
+	return (0);
 }
