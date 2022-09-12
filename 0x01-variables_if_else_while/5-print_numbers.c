ca915938f65f8a8adb3af222c2a5997c13fa0024
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-12 10:52:49 -0700

    6th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	num++;
+
+	putchar('\n');
+	return (0);
+}
