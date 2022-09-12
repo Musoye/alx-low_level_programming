f526668edf48b26361b2582f90f32023508603f0
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-12 13:29:54 -0700

    do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num1 < 8; num++)
+	for (num1 = 0; num1 < 8; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
@@ -21,7 +21,7 @@ int main(void)
 				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
 
-				putchhar(',');
+				putchar(',');
 				putchar(' ');
 			}
 		}
