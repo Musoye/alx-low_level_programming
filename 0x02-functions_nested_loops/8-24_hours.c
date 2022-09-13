e57431c519ddacb6c5ca273538d325a4e35dfa89
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-13 14:48:45 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ * Return: null
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	int a = 9;
+
+	h2 = 0;
+
+	while (h2 <= 2)
+	{
+		if (h2 == 2)
+		{
+			a = 3;
+		}
+		h1 = 0;
+
+		while (h1 <= a)
+		{
+			m2 = 0;
+			
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
+	}
+}
