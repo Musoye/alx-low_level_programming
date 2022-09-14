996c863f46f0c1ddf4e3b46e0cccc41b47c92f53
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-14 03:37:48 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,16 @@
  */
 void print_to_98(int n)
 {
+	int a;
+
 	if (n > 98)
 	{
-		for (n; n >= 98; n--)
-			printf9"%d", n);
+		for (a = n; a >= 98; a--)
+			printf("%d", a);
 	}
 	else
 	{
-		for (n; n <= 98; n++)
-			printf("%d", n);
+		for (a = n; a <= 98; a++)
+			printf("%d", a);
 	}
 }
