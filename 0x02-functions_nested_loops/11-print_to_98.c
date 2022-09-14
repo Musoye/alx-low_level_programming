8efb784ab4e1a4419a883258c18ad505b0aa739b
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-14 03:32:17 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - entry point
+ * Description; addition fron number n to 98 and print iy ou
+ * @n: the input number
+ * Return: null
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n; n >= 98; n--)
+			printf9"%d", n);
+	}
+	else
+	{
+		for (n; n <= 98; n++)
+			printf("%d", n);
+	}
+}
