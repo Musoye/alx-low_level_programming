989dd48427e5a91e948cfe9f199bd710e9bcbc46
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-15 03:07:07 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -entry point
+ * Description: to test the function for 0
+ * @i: the inputted element to test whether positive or negative
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
