7bf8fff1de1a351d1916a385d6926438bf4d365b
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-15 18:11:27 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,27 +8,22 @@
 
 void print_triangle(int size)
 {
-	int a = 1, b;
+	int a, b;
 
-	while (a <= size && size > 0)
+	for (a = 0; a < size; a++)
 	{
-		b = 0;
-		while (b < size - 1)
+		for (b = 1; b < (size - 1); b++)
 		{
 			_putchar(' ');
-			b++;
 		}
-		b = 0;
-		while (b < a)
+		for (b--; b < size; b++)
 		{
 			_putchar('#');
-			b++;
 		}
-		_putchar('\n');
-		a++;
+		if (a < (size - 1))
+		{
+			_putchar('\n');
+		}
 	}
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
