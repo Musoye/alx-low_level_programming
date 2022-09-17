d4c65c5ec9670492d549ed2bc85bf088b3d5ef84
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-17 09:43:04 -0700

    Never be afraid to try. The only angle through which you can approach success is by the "try-angle"

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_triangle - entry point to print a right angled triangle
@@ -13,7 +12,7 @@ void print_triangle(int size)
 
 	for (a = 0; a < size; a++)
 	{
-		for (b = 1; b < (size - 1); b++)
+		for (b = 1; b < (size - a); b++)
 		{
 			_putchar(' ');
 		}
