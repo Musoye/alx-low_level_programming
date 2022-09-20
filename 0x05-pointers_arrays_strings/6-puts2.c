f4542cd935b237c4ec3a385451ee17f5d5447b8b
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-20 15:47:15 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - print even element
+ * @str: the string
+ * Return: null
+ */
+void puts2(char *str)
+{
+	int i, len = 0;
+
+	while (str[i])
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i = i + 2)
+	{
+		putchar(str[i]);
+	}
+}
