a602d56b1c029a7816ed13d7e9009ef75a176121
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-21 09:26:05 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenation of string
+ * @dest: the first string
+ * @src: the second string
+ * Return: dest which is concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t len = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is more pointer array and string directory
