c74c163019655eec30a55f9a191aa790c75b4f5b
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-21 09:41:40 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_strcat - concatenation of string
+ * *_strncat - concatenation of string
  * @dest: the first string
  * @src: the second string
+ * @n: the byte of src
  * Return: dest which is concatenated string
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	size_t len = strlen(dest);
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
 	}
