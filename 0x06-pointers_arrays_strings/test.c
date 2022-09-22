4e19521db6ca7d4f846727421c9a19beccae3dc3
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-22 09:21:44 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/test.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+char *p;
+
+p = leet(s);
+printf("%s", p);
+printf("%s", s);
+return (0);
+}
