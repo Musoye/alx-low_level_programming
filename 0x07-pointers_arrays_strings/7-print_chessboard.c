b306ccfb5a95f77fc2141d3b1783af733f58cd64
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-27 03:11:30 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - print inputted substring in an array
+ * @a: input pointer
+ * Return: Null
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	b = 0;
+	c = 0;
+	while (b < 64)
+	{
+		if (b % 8 == 0 && b != 0)
+		{
+			c = b;
+			_putchar('/n');
+		}
+		_putchar(a[b / 8][b - c]);
+		b++;
+	}
+	_putchar('/n');
+}
