470947d2e156c64107cdeb52ea24bce8b274b60d
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-27 02:27:04 -0700

    memst

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @n: number of byte of memory to be fill with
  * Return: the string
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
