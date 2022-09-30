e3b03dbc66cac218ea4fa8d36c964529acf186a2
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-09-30 02:34:34 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - change the name of the unction
+ * @argc: the lendth of argument
+ * @argv: the array of string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+main argument directory
