e86293eaa7f57d7d3a56d8f0c82cd4b9a2d303bc
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-06 05:33:59 -0700

    submit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+char *_memset(char *s, char b, unsigned int n);
 /**
  * _calloc - recreates calloc function
  * @nmemb: the number of elments
@@ -9,14 +10,28 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *p;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == 0)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = '0';
+	_memset(p, 0, nmemb * size);
 	return (p);
+}
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
 }
