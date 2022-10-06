c742d29f9d67b56de455010cf722ac1019cc82d1
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-06 04:21:09 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - recreates calloc function
+ * @nmemb: the number of elments
+ * @size: the size of the member of the array
+ * Return: return pointer to the allocate dmemory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = '0';
+	return (p);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@
 #include <stdio.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /**MAIN_H*/
