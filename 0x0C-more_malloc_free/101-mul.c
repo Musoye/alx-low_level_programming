452a1b5fc3baae6aebcc8e328f1f50431b36b950
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-06 06:37:15 -0700

    submit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiply two numbers
+ * @argc: the number of argument
+ * @argv: the argument vector
+ * Return: 0 always
+ */
+int main(argc, char *argv[])
+{
+	unsigned long int a;
+	unsigned long int b;
+
+	if (argc != 3)
+	{
+		_putchar("Error\n");
+		exit(98);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%lu\n", a * b);
+	}
+	return (0);
+}
