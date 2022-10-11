1a370f9bf7343b4f67d41fa536332b313c95c7bc
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-11 03:31:39 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,68 @@
-#include <stdio.h>
 #include "dog.h"
-
+#include <stdlib.h>
+int _strlen(char *s);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 /**
- * new_dog - fuction creates new struct
- * @name: string
- * @age: float
- * @owner: string
- * Return: void
+ * _strlen - lengths of a string
+ * @s: the string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * _strcopy - copy string to one another
+ * @dest: the copied string
+ * @src: the string to copy
+ * Return: dest
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int ind = 0;
+
+	for ( ; src[ind] != '\0'; ind++)
+	{
+		dest[ind] = src[ind];
+	}
+	dest[ind] = '\0';
+	return (dest);
+}
+/**
+ * new_dog - to create new dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Return: the new struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (*new_dog == NULL)
+	dog_t *newdogi;
+
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	newdogi = malloc(sizeof(dog_t));
+	if (newdogi == NULL)
+		return (NULL);
+	newdogi->name = malloc(sizeof(char) * _strlen(name) + 1);
+	if (newdogi->name == NULL)
+	{
+		free(newdogi);
+		return (NULL);
+	}
+	newdogi->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+	if (newdogi->owner == NULL)
+	{
+		free(newdogi);
+		return (NULL);
+	}
+	newdogi->name = _strcopy(newdogi->name, name);
+	newdogi->age = age;
+	newdogi->owner = _strcopy(newdogi->owner, owner);
+	return (newdogi);
 }
