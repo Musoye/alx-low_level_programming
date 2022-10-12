077ea6dd4af30d6c5587135c328b691ece47872b
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-12 05:33:15 -0700

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+/**
+ * int_index - chevck if a number is equal or not
+ * @array: the array
+ * @size: the size
+ * @cmp: the function pointer
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	for ( ; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
