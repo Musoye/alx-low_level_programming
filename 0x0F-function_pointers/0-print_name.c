570981553bf1bd54d7a436a7a40939e89efe0a0c
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-12 04:40:51 -0700

    message

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,6 +8,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	(*f)(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && size > 0; action != NULL)
+	if (array != NULL && size > 0 && action != NULL)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
