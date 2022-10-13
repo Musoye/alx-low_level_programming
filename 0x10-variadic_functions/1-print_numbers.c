d5b9eef72b61a47f35ec89d0cda556f82bfe265e
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-13 05:59:12 -0700

    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "variadic_functions.h"
 /**
  * print_numbers- function to print unrevocked number
  * of numbers
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-int _putchar(char c);
 
 #endif /** VARIADIC_FUNCTIONS_H */
