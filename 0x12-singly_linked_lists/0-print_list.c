89874724b969e623d3d3fa45271b79f6623af39c
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-20 04:42:12 -0700

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,11 +6,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_num = 1;
+	size_t node_num = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] (%s)\n", 0, "nil");
@@ -21,6 +19,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		node_num++;
 	}
-	/**printf("[%d] (%s)\n", h->len, h->str);*/
 	return (node_num);
 }
