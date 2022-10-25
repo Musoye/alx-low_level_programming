90799de62a5e1a42b6c3002a3d6df26b21d4d5fb
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-25 02:17:25 -0700

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - free linked list
+ * @head: free head of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
