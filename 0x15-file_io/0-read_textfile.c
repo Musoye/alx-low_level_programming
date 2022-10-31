4d0bdbe75e5d5d3365e40ce4f0016171fc0e5715
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-10-31 06:39:25 -0700

    hmmm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,44 @@
 #include "main.h"
+
 /**
- * read_textfile - read the text file
- * @filename: the file name
- * @letters: he size of letters
- * Return: size
- */
+* read_textfile - a function that reads a text file
+*and prints it to the POSIX standard output
+* @filename: the file name
+* @letters: the number of letters
+* Return: the actual number of letters or 0 if fails
+**/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, length, len, i;
+	int f, length, i, res;
 	char *buffer;
 
 	if (filename == NULL)
-		return (-1);
-	/** open **/
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
 		return (0);
-	}
+	/* open */
+
+	f = open(filename, O_RDONLY);
+
+	if (f == -1)
+		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-	/**read**/
-	read(fd, buffer, letters);
+	if (!buffer)
+		return (0);
+
+	read(f, buffer, letters);
 	buffer[letters] = '\0';
-	for (i = 0; buffer[i] != '\0'; i++)
-		len = +1;
-	length = close(fd);
-	if (length != 0)
+
+	for (i = 0; buffer[i] != '\0'; i += 1)
+		length += 1;
+
+	res = close(f);
+	if (res != 0)
 		exit(-1);
-	length = write(STDOUT_FILENO, buffer, len);
-	if (length != len)
+	res = write(STDOUT_FILENO, buffer, length);
+	if (res != length)
 		return (0);
 	free(buffer);
-	return (len);
+
+	return (length);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <fcnt1.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-#endif /**MAIN_H*/
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void error_98(int f0, char *buffer, char *argv);
+void error_99(int f0, char *buffer, char *argv);
+void error_100(int f0, char *buffer);
+
+#endif/**MAIN_H*/
