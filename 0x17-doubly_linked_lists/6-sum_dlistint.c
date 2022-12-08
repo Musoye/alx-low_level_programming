989319bf5db7878065f48e2d56218c6629823713
oyebamiji mustapha oyetunde <oyebamijimustapha44@gmail.com>
2022-12-08 06:45:30 -0800

    Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum element of a dbl
+ * @head: the head of doubly linked list
+ * Return: the sum or 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != 0)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
