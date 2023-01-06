b221fbdbdbdf537a2e269c8b2f90889e9f46c9b2
musoye <oyebamijimustapha44@gmail.com>
2023-01-06 06:57:45 -0800

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/*
+ * hash_table_create - create has table
+ * @size: the size of ther hash table
+ * Return: the table else NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+A repository on hash datastructure.whinch include how to use hash
+function to generate id for the key to store with the value in the
+hash table
