8e81ee43a2b2937dcc26f59b3a9663ca7e791f6c
musoye <oyebamijimustapha44@gmail.com>
2023-01-06 08:08:04 -0800

    >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add element into hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value to keep
+ * Return: 1 on success 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *new_value;
+	unsigned long int index, i;
+
+	if (ht == NULL || value == NULL || *key == '\0' || key == NULL)
+	{
+		return (0);
+	}
+	new_value = strdup(value);
+	if (new_value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = new_value;
+			return (1);
+		}
+	}
+	new = malloc(hash_node_t);
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0)
+	}
+	new->value = value;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
