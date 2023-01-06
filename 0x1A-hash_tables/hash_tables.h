c8a4757336578a5ad11baa8e20707a59ef83e446
musoye <oyebamijimustapha44@gmail.com>
2023-01-06 07:15:56 -0800

    djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,4 +31,5 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif/** hash_tables_h*/
